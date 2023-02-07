cf2d0cf67adbee2b99a4d091cd7eb831974da0b1
Administrator <mukeshreus21@gmail.com>
2023-02-08 00:24:16 +0530

    bottom navigation problem partially resolved
    MUKESH JOSE  08/02/2023   00:23

--- app/src/main/java/com/example/notificationdrawerandbottomnavigation/SampleActivity.java
+++ app/src/main/java/com/example/notificationdrawerandbottomnavigation/SampleActivity.java
@@ -14,7 +14,8 @@ import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
-public class SampleActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class SampleActivity extends AppCompatActivity {
+    //public class SampleActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     BottomNavigationView bottomNavigationView;
     BlankFragmentOne blankFragmentOne = new BlankFragmentOne();
     BlankFragmentTwo blankFragmentTwo = new BlankFragmentTwo();
@@ -26,47 +27,48 @@ public class SampleActivity extends AppCompatActivity implements BottomNavigatio
         setContentView(R.layout.activity_sample);
 
 
-        bottomNavigationView = findViewById(R.id.bottom_navigation_view_sample);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(this);
-//        bottomNavigationView.setSelectedItemId(R.id.item1);
-
-    }
-
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-        switch (item.getItemId()) {
-            case R.id.item1:
-                getSupportFragmentManager().beginTransaction().replace(R.id.sample_fragment, blankFragmentOne).commit();
-//                FragmentManager fragmentOneManagerSample = getSupportFragmentManager();
-//                FragmentTransaction fragmentOneTransactionSample = fragmentOneManagerSample.beginTransaction();
-//                fragmentOneTransactionSample.replace(R.id.main_fragment, blankFragmentOne);
-//                fragmentOneTransactionSample.addToBackStack(null);
-//                fragmentOneTransactionSample.commit();
-                return true;
-
-
-            case R.id.item2:
-                getSupportFragmentManager().beginTransaction().replace(R.id.sample_fragment, blankFragmentTwo).commit();
-//                FragmentManager fragmentTwoManagerSample = getSupportFragmentManager();
-//                FragmentTransaction fragmentTwoTransactionSample = fragmentTwoManagerSample.beginTransaction();
-//                fragmentTwoTransactionSample.replace(R.id.main_fragment, blankFragmentTwo);
-//                fragmentTwoTransactionSample.addToBackStack(null);
-//                fragmentTwoTransactionSample.commit();
-
-                return true;
-
-            case R.id.item3:
-                getSupportFragmentManager().beginTransaction().replace(R.id.sample_fragment, blankFragmentThree).commit();
-//                FragmentManager fragmentThreeManager = getSupportFragmentManager();
-//                FragmentTransaction fragmentThreeTransaction = fragmentThreeManager.beginTransaction();
-//                fragmentThreeTransaction.replace(R.id.main_fragment, blankFragmentThree);
-//                fragmentThreeTransaction.addToBackStack(null);
-//                fragmentThreeTransaction.commit();
-                return true;
-        }
-        return false;
+//        bottomNavigationView = findViewById(R.id.bottom_navigation_view_sample);
+//
+//        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+////        bottomNavigationView.setSelectedItemId(R.id.item1);
+//
+//    }
+//
+//
+//    @Override
+//    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//
+//        switch (item.getItemId()) {
+//            case R.id.item1:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.sample_fragment, blankFragmentOne).commit();
+////                FragmentManager fragmentOneManagerSample = getSupportFragmentManager();
+////                FragmentTransaction fragmentOneTransactionSample = fragmentOneManagerSample.beginTransaction();
+////                fragmentOneTransactionSample.replace(R.id.main_fragment, blankFragmentOne);
+////                fragmentOneTransactionSample.addToBackStack(null);
+////                fragmentOneTransactionSample.commit();
+//                return true;
+//
+//
+//            case R.id.item2:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.sample_fragment, blankFragmentTwo).commit();
+////                FragmentManager fragmentTwoManagerSample = getSupportFragmentManager();
+////                FragmentTransaction fragmentTwoTransactionSample = fragmentTwoManagerSample.beginTransaction();
+////                fragmentTwoTransactionSample.replace(R.id.main_fragment, blankFragmentTwo);
+////                fragmentTwoTransactionSample.addToBackStack(null);
+////                fragmentTwoTransactionSample.commit();
+//
+//                return true;
+//
+//            case R.id.item3:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.sample_fragment, blankFragmentThree).commit();
+////                FragmentManager fragmentThreeManager = getSupportFragmentManager();
+////                FragmentTransaction fragmentThreeTransaction = fragmentThreeManager.beginTransaction();
+////                fragmentThreeTransaction.replace(R.id.main_fragment, blankFragmentThree);
+////                fragmentThreeTransaction.addToBackStack(null);
+////                fragmentThreeTransaction.commit();
+//                return true;
+//        }
+//        return false;
+//    }
     }
 }
